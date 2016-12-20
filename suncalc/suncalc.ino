34b484e2465ff0a3b4a66c3b34cc465e7c54d5f1
Sibren Talens <sibrentalens@gmail.com>
2016-12-20 15:07:33 +0100

    Fix arduino

--- suncalc/suncalc.ino
+++ suncalc/suncalc.ino
@@ -1,4 +1,5 @@
 char buffer[128];
+int cursor;
 
 /**
  * @brief           Zet de zonnepanelen in het prototype in de juiste positie
@@ -9,36 +10,41 @@ char buffer[128];
  * @param altitude  De altitude
  */
 void fixPanel(double azimuth, double altitude) {
-
-  Serial.println("This is temporary");
-
   Serial.print(azimuth, DEC);
   Serial.print("\t");
   Serial.print(altitude, DEC);
   Serial.print('\n');
 }
 
+char delim = '\t';
+
+char *strtokbuf;
+
 void setup() {
+        cursor = 0;
+        
+        strtokbuf = (char *) malloc(128);
 
 	Serial.begin(9600);
 
 	while(!Serial); // Volgensmij is dit niet nodig
 }
 
-int cursor = 0;
-
 void loop() {
   if (Serial.available()) {
     byte incoming = Serial.read();
 
     if(incoming != '\n') {
-      
       buffer[cursor++] = incoming;
     } else {
       double azimuth, altitude;
-
-      sscanf("%f\t%f", buffer, &azimuth, &altitude);
-
+      
+      char *first_part = strtok_r(buffer, &delim, &strtokbuf);
+      char *second_part = strtok_r(0, &delim, &strtokbuf);
+      
+      azimuth = atof(first_part);
+      altitude = atof(second_part);
+      
       fixPanel(azimuth, altitude);
 
       cursor = 0;
